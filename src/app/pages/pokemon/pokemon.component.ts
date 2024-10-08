f8de046ca14963f6aa75915d9565118026894ab0
josemm0505 <mejiamicoltalol@gmail.com>
2024-10-07 22:07:16 -0500

    Busqueda agregada

--- src/app/pages/pokemon/pokemon.component.ts
+++ src/app/pages/pokemon/pokemon.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CardComponent } from './card/card.component';
-import { Pokemons } from './interfaces/pokemons';
+import { Pokemons} from './interfaces/pokemons';
 import { PokemonService } from './services/pokemon.service';
 import { PaginacionComponent } from './paginacion/paginacion.component';
+import { SearchComponent } from './search/search.component';
 
 @Component({
   selector: 'app-pokemon',
   standalone: true,
-  imports: [CardComponent, PaginacionComponent],
+  imports: [CardComponent, PaginacionComponent, SearchComponent],
   templateUrl: './pokemon.component.html',
   styleUrl: './pokemon.component.css'
 })
@@ -38,6 +39,30 @@ export class PokemonComponent implements OnInit{
 
   setNewPokemon(pokemonsNews: Pokemons):void{
     this.pokemons = pokemonsNews;
+  }
+
+  searchPokemon(termino: string):void{
+    if(termino){
+      this.srvPokemon.getpokemon(termino).subscribe((pokemon)=>{
+        this.pokemons ={
+          count: 1,
+          next: '',
+          previous: null,
+          results:[
+            {
+              name: pokemon.name,
+              url:'',
+              data: pokemon
+          }
+        ]
+        };
+        this.srvPokemon.nextUrl = null;
+        this.srvPokemon.previousUrl = null;
+      });
+    }else{
+      this.ngOnInit();
+    }
+
   }
 
 }
